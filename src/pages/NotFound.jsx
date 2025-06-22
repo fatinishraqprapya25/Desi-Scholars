b1e1ad635eca0f79af8ddf018d43e5fc75aafe9f
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-22 16:57:37 +0600

    four_zero_four page optimized

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,25 +3,23 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
     return (
         <>
-            <section class="page_404">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-12 ">
-                            <div class="col-sm-10 col-sm-offset-1  text-center">
-                                <div class="four_zero_four_bg">
-                                    <h1 class="text-center ">404</h1>
-
-
+            <section className="page_404" style={{ fontFamily: 'Arial, sans-serif' }}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 ">
+                            <div className="col-sm-10 col-sm-offset-1 text-center">
+                                <div className="four_zero_four_bg">
+                                    <h1 className="text-center ">404</h1>
                                 </div>
 
-                                <div class="contant_box_404">
-                                    <h3 class="h2">
+                                <div className="contant_box_404">
+                                    <h3 className="h2">
                                         Look like you're lost
                                     </h3>
 
                                     <p>the page you are looking for not avaible!</p>
-
-                                    <Link to="/" class="link_404">Go to Home</Link>
+                                    <br />
+                                    <Link to="/" className="text-white px-4 py-3 rounded shadow-sm bg hover:bg-indigo-700 duration-1 bg-indigo-500">Go to Home</Link>
                                 </div>
                             </div>
                         </div>
@@ -29,5 +27,5 @@ export default function NotFound() {
                 </div>
             </section>
         </>
-    )
+    );
 }
