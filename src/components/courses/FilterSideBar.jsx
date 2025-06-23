aa1bdb5cb9eafcfb0d15c989b8ce3ff9343b55f6
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-23 23:31:23 +0600

    load courses after clearing filter done

--- src/components/courses/FilterSideBar.jsx
+++ src/components/courses/FilterSideBar.jsx
@@ -24,6 +24,7 @@ const FilterSidebar = ({ applyFilter }) => {
         setSearchText("");
         setSelectedLevel("");
         setSelectedPrice("");
+        applyFilter({});
     };
 
     return (
