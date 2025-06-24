b8ca083776fcbaaa330c3a5ae2c48a990e22eba1
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-24 15:36:17 +0600

    fetching enrolled courses and practice tests done

--- src/components/dashboards/student/home/KeyMetrics.jsx
+++ src/components/dashboards/student/home/KeyMetrics.jsx
@@ -1,12 +1,51 @@
 import { motion } from "framer-motion";
 import { BookOpen, Trophy, CalendarDays, Award, BarChart2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import validateToken from "../../../../utils/ValidateToken";
 
 export default function KeyMetrics() {
+    const [enrolledCourses, setEnrolledCourses] = useState(0);
+    const [tests, setTests] = useState(0);
+
+    const fetchEnrolledCourses = async () => {
+        const userInfo = await validateToken();
+        const response = await fetch(`http://localhost:5000/api/courses/user/${userInfo.id}`);
+        if (!response.ok) {
+            alert('error occured fetching course count');
+            return;
+        }
+        const result = await response.json();
+        if (!result.success) {
+            alert(result.message);
+            return;
+        }
+        setEnrolledCourses(result.data.length);
+    }
+
+    const fetchPracticeTests = async () => {
+        const response = await fetch(`http://localhost:5000/api/tests/`);
+        if (!response.ok) {
+            alert('error occured fetching course count');
+            return;
+        }
+        const result = await response.json();
+        if (!result.success) {
+            alert(result.message);
+            return;
+        }
+        setTests(result.data.length);
+    }
+
+    useEffect(() => {
+        fetchEnrolledCourses();
+        fetchPracticeTests();
+    }, []);
+
     const metrics = [
-        { id: 1, name: 'Enrolled Courses', value: 3, icon: <BookOpen className="h-8 w-8 text-blue-600" />, color: 'from-blue-100 to-blue-200' },
-        { id: 2, name: 'Courses Completed', value: 7, icon: <Trophy className="h-8 w-8 text-green-600" />, color: 'from-green-100 to-green-200' },
-        { id: 3, name: 'Upcoming Deadlines', value: 2, icon: <CalendarDays className="h-8 w-8 text-orange-600" />, color: 'from-orange-100 to-orange-200' },
-        { id: 4, name: 'Achievements Unlocked', value: 12, icon: <Award className="h-8 w-8 text-purple-600" />, color: 'from-purple-100 to-purple-200' },
+        { id: 1, name: 'Enrolled Courses', value: enrolledCourses, icon: <BookOpen className="h-8 w-8 text-blue-600" />, color: 'from-blue-100 to-blue-200' },
+        { id: 2, name: 'Courses Completed', value: 0, icon: <Trophy className="h-8 w-8 text-green-600" />, color: 'from-green-100 to-green-200' },
+        { id: 3, name: 'Practice Tests', value: tests, icon: <CalendarDays className="h-8 w-8 text-orange-600" />, color: 'from-orange-100 to-orange-200' },
+        { id: 4, name: 'Resources', value: 12, icon: <Award className="h-8 w-8 text-purple-600" />, color: 'from-purple-100 to-purple-200' },
     ];
 
     const itemVariants = {
