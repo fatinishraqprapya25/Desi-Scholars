dac26e93ea2af00709b6d7e6f3a46015a3a02a06
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-24 15:38:25 +0600

    fetching resources and keymatrics basics done

--- src/components/dashboards/student/home/KeyMetrics.jsx
+++ src/components/dashboards/student/home/KeyMetrics.jsx
@@ -6,6 +6,7 @@ import validateToken from "../../../../utils/ValidateToken";
 export default function KeyMetrics() {
     const [enrolledCourses, setEnrolledCourses] = useState(0);
     const [tests, setTests] = useState(0);
+    const [resources, setResources] = useState(0);
 
     const fetchEnrolledCourses = async () => {
         const userInfo = await validateToken();
@@ -25,7 +26,7 @@ export default function KeyMetrics() {
     const fetchPracticeTests = async () => {
         const response = await fetch(`http://localhost:5000/api/tests/`);
         if (!response.ok) {
-            alert('error occured fetching course count');
+            alert('error occured fetching resources');
             return;
         }
         const result = await response.json();
@@ -36,16 +37,31 @@ export default function KeyMetrics() {
         setTests(result.data.length);
     }
 
+    const fetchResources = async () => {
+        const response = await fetch(`http://localhost:5000/api/resource/`);
+        if (!response.ok) {
+            alert('error occured fetching course count');
+            return;
+        }
+        const result = await response.json();
+        if (!result.success) {
+            alert(result.message);
+            return;
+        }
+        setResources(result.data.length);
+    }
+
     useEffect(() => {
         fetchEnrolledCourses();
         fetchPracticeTests();
+        fetchResources();
     }, []);
 
     const metrics = [
         { id: 1, name: 'Enrolled Courses', value: enrolledCourses, icon: <BookOpen className="h-8 w-8 text-blue-600" />, color: 'from-blue-100 to-blue-200' },
         { id: 2, name: 'Courses Completed', value: 0, icon: <Trophy className="h-8 w-8 text-green-600" />, color: 'from-green-100 to-green-200' },
         { id: 3, name: 'Practice Tests', value: tests, icon: <CalendarDays className="h-8 w-8 text-orange-600" />, color: 'from-orange-100 to-orange-200' },
-        { id: 4, name: 'Resources', value: 12, icon: <Award className="h-8 w-8 text-purple-600" />, color: 'from-purple-100 to-purple-200' },
+        { id: 4, name: 'Resources', value: resources, icon: <Award className="h-8 w-8 text-purple-600" />, color: 'from-purple-100 to-purple-200' },
     ];
 
     const itemVariants = {
