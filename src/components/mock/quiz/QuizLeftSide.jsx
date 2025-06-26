565e956bc9e7b73f552817a746420541ba233c7c
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-26 22:29:18 +0600

    module leftside done

---
+++ src/components/mock/quiz/QuizLeftSide.jsx
@@ -0,0 +1,7 @@
+export default function QuizLeftSide() {
+    return (
+        <div>
+            Researchers and conservationists stress that biodiversity loss due to invasive species is ______blank. For example, people can take simple steps such as washing their footwear after travel to avoid introducing potentially invasive organisms into new environments.
+        </div>
+    )
+}
--- src/pages/MockQuestion.jsx
+++ src/pages/MockQuestion.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import QuizHeader from "../components/mock/quiz/QuizHeader";
+import QuizLeftSide from "../components/mock/quiz/QuizLeftSide";
 
 export default function MockQuestion() {
     const { id } = useParams();
     const [mockQuestions, setMockQuestions] = useState();
-    useEffect(() => {
+    const fetchMockQuestions = async () => {
 
+    }
+    useEffect(() => {
+        fetchMockQuestions();
     }, []);
 
     return (
         <div>
             <QuizHeader moduleName="English 1" initialMinutes={2} initialSeconds={0} />
+            <div className="grid grid-cols-2 ps-0 md:ps-15 mt-0 md:mt-15">
+                <QuizLeftSide />
+            </div>
         </div>
     )
 }
