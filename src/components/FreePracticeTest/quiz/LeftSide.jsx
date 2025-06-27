e41232085b8cdc6696f050c0d2aa38e577ca0c36
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-28 04:14:16 +0600

    the left side of the quiz section optimized

--- src/components/FreePracticeTest/quiz/LeftSide.jsx
+++ src/components/FreePracticeTest/quiz/LeftSide.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 export default function LeftSide({ question, length, meta, changeMeta }) {
     return <div className="min-h-screen p-6">
         <div className="rounded-lg p-6">
-            <p className="text-gray-700">
+            <p className="text-gray-700 text-lg">
                 {question && question.explanation}
             </p>
         </div>
